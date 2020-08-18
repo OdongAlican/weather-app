4303ddcdddc700f35f0cb5738837e30e7c0ad5e6
OdongAlican <sandieo.2020@gmail.com>
2020-08-18 18:22:39 +0300

    update the main weather section

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 
 const displayData = (data) => {
   if (data) {
-    console.log(data.main);
+    console.log(data);
+    document.getElementById('city-heading').innerHTML = data.name;
+    document.getElementById('feels-like-id').innerHTML = data.main.feels_like;
+    document.getElementById('humidity-temp-id').innerHTML = data.main.humidity;
+    document.getElementById('pressure-temp-id').innerHTML = data.main.pressure;
+    document.getElementById('max-temp-id').innerHTML = data.main.temp_max;
+    document.getElementById('min-temp-id').innerHTML = data.main.temp_min;
+
+    const imageSection = document.querySelector('.gif-section');
+    document.getElementById('weather-span').innerHTML = data.weather[0].main
+    document.getElementById('weather-description').innerHTML = data.weather[0].description
+    console.log(data.weather[0].main);
+    const image = document.createElement('img');
+    image.setAttribute('class', 'image-giff');
+
+    if (data.weather[0].main === 'Rain') {
+      imageSection.innerHTML = '';
+      image.setAttribute('src', 'images/rainy.png');
+      imageSection.appendChild(image);
+    } else if (data.weather[0].main === 'Clouds') {
+      imageSection.innerHTML = '';
+      image.setAttribute('src', 'images/cloudy.png');
+      imageSection.appendChild(image);
+    } else if (data.weather[0].main === 'Clear') {
+      imageSection.innerHTML = '';
+      image.setAttribute('src', 'images/clear.png');
+      imageSection.appendChild(image);
+    } else if (data.weather[0].main === 'Mist') {
+      imageSection.innerHTML = '';
+      image.setAttribute('src', 'images/mist.gif');
+      imageSection.appendChild(image);
+    }else if (data.weather[0].main === 'Thunderstorm') {
+      imageSection.innerHTML = '';
+      image.setAttribute('src', 'images/Thunderstorm.png');
+      imageSection.appendChild(image);
+    }else if (data.weather[0].main === 'Drizzle') {
+      imageSection.innerHTML = '';
+      image.setAttribute('src', 'images/rainGif.png');
+      imageSection.appendChild(image);
+    }else if (data.weather[0].main === 'Snow') {
+      imageSection.innerHTML = '';
+      image.setAttribute('src', 'images/drizzle.png');
+      imageSection.appendChild(image);
+    }
   }
 };
 
@@ -9,19 +52,19 @@ const mainApp = async (cityName) => {
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=bae266a2bf0e92ca7ec62610275967dc`;
 
   try {
+    document.getElementById('spinner-section').classList.add('loader');
+    document.querySelector('.weather-content').classList.add('hide-weather-content');
     const response = await fetch(url, { mode: 'cors' });
     const data = await response.json();
-    document.getElementById('spinner-section').classList.add('loader');
     setTimeout(() => {
       document.getElementById('spinner-section').classList.remove('loader');
       document.querySelector('.weather-content').classList.remove('hide-weather-content');
       displayData(data);
-    }, 2000);
+    }, 1000);
   } catch (error) {
     return error;
   }
 };
-
 
 document.getElementById('search-button').addEventListener('click', () => {
   const city = document.getElementById('city-name').value;
